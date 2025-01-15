a641a616f48ec7728bfa74ac47de1136da3fd9f1
Debasish Ranjan Nath <debasish.n@useready.com>
2025-01-15 18:41:36 +0530

    Modified code to use

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
+  origin: "https://webappwithadminpanel.netlify.app/",
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
